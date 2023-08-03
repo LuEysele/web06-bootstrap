ef464466461990f3a74b7a76defc9e2dba02f05b
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-03 17:26:12 -0600

    feat: uso de botón para cambiar colores

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@ console.log("Página en construcción con Link");
 function clickPatricio() {
     alert("¿La mayonesa es un instrumento?");
 }
+
+function changeColor (elementHtml, color) {
+   // alert("¡Qué emoción! Voy a cambiar mi color")
+   console.log(elementHtml);
+   console.log(color);
+   elementHtml.style.color = color;
+}
