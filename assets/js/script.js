8649016ece787b0925bd4ba63f0a8a46d8abef60
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-04 11:21:18 -0600

    feat: uso de botón para cambiar colores

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,28 @@ function changeColor (elementHtml, color) {
    console.log(elementHtml);
    console.log(color);
    elementHtml.style.color = color;
+
+}
+
+function changeColorWithPrompt(elementHtml) {
+    const color = prompt("Dime el color en inglés", "yellow");
+    changeColor( elementHtml, color );
+    elementHtml.innerHTML = "<h3>Ahora soy <em>" + color  + "</em> </h3>"
+}
+
+function changeTextOfUserColor( element, color ){
+    element.innerHTML = "<h3>Ahora soy <em> " + color + "</em> </h3>"
+}
+
+function resetColor() {
+    const refRedColor = document.getElementById("red-color");
+    const refGreenColor = document.getElementById("green-color");
+    const refPurpleColor = document.getElementById("purple-color");
+    const refuserColor = document.getElementById("user-color");
+
+    changeColor(refGreenColor, 'black');
+    changeColor(refPurpleColor, 'black');
+    changeColor(refRedColor, 'black');
+    changeColor(refuserColor, 'black');
+    changeTextOfUserColor( refuserColor, 'black');
 }
