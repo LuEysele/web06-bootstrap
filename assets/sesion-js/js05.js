29b12a1b8b6b261c30a93100bd78c5645c1f5665
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-14 15:37:17 -0600

    ci: objetos

--- assets/sesion-js/js05.js
+++ assets/sesion-js/js05.js
@@ -131,4 +131,79 @@ console.log( `nombre: ${person["name"]}`);
 
 const occupation = person.occupation || "Software Engineer";
 
-console.log(`Ocupación: ${occupation}`)
+console.log(`Ocupación: ${occupation}`);
+
+//------------ Comparación estricta -----------------
+/* 
+ === Estrictamente igual, se debe comparar el valor y el tipo de dato
+ !== Estrictamente diferente, debe ser diferente en valor y tipo
+
+*/
+
+console.log( "23" === 23); // false
+console.log( "true" === true ); // false
+console.log( "true" == true ); // false NaN == 1
+console.log( "45" !== 45); //  true
+
+//---------- Operador de desestructuración (desturcturing) -----------
+/* 
+ Es una expresión en JS que hace posible extraer datos de arreglos u objetos y nos ayuda a realizar asignaciones más complejas.
+
+*/
+
+let dataA = 10;
+let dataB = 20;
+console.log(`a: ${dataA}, b: ${dataB}`); // 10, 20
+//Intercambiar los datos -> A: 20 y B: 10
+//[dataA, dataB] = [dataB, dataA];
+
+/* let temp = dataA;
+dataA = dataB
+dataB = temp; */
+
+[ dataA, dataB ] = [ dataB, dataA ];
+console.log(`a: ${dataA}, b: ${dataB}`) // 10, 20
+
+const car = {
+    model : "vento",
+    year: 2023,
+    brand : "VW",
+    color : "red",
+    price : 320_000,
+    additionalInfo : {
+        bluetooth : true,
+        cruiseControl : true,
+        gps : false,
+    }
+}
+
+// console.log(`Modelo : ${ car.model }`); // Vento
+// console.log(`Año : ${ car.year}`); // 2023
+/* const carModel = car.model;
+const carYear = car.year;
+console.log(`Modelo : ${ carModel }`); // Vento
+console.log(`Año : ${ carYear}`); // 2023 */
+
+const { year, model : carModel, additionalInfo : {bluetooth} } = car;
+console.log(`Modelo: ${ carModel }, Año: ${ year }`) // Vento, 2023
+console.log(`Tiene bluetooth: ${ bluetooth ? "sí" : "no"}`) // Vento, 2023
+
+
+//------------- ejercicio --------------------
+/* 
+  Hacer una función que realice la división de 2 números
+  y entregue (return) en entero el cociente y residuo
+  
+  Llamar a la función y mostrar en la consola el resultado
+
+*/
+const num1 = 5;
+const num2 = 2;
+
+function divEntero(num1, num2) {
+    let division = parseInt(num1 / num2);
+    let residuo = num1 % num2;
+    return {division, residuo};
+}
+
+console.log(`División ${divEntero(num1,num2).division}, residuo: ${divEntero(num1,num2).residuo}`)
