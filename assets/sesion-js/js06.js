20d49f48b0fc4e2d032a24b073bef557d94f0873
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-14 17:04:50 -0600

    feat: Manipulación del DOM

---
+++ assets/sesion-js/js06.js
@@ -0,0 +1,66 @@
+console.log("Sesión JS06: Manipulación del DOM");
+
+/* 
+ * Modificar un elemento HTML por medio de su ID
+*/
+const findElementById = () => {
+    const title = document.getElementById("title");
+    console.log(title);
+    console.log(typeof title);
+    console.log(title.innerHTML); // Sesión JS06
+    //title.innerHTML = `Generation - Sesión JS 06`;
+    title.innerHTML = ` <span class="text-primary"> Generation </span> - Sesión JS 06 `;
+}
+
+findElementById();
+
+//---------------- Encontrar elementos por etiqueta ----------------------
+
+const changeElementsByName = () =>{
+    const unorderList = document.getElementsByTagName("li"); // Colección de elementos
+    console.log( unorderList ); // HTML Collection
+
+    for( const listItem of unorderList){
+        listItem.innerHTML = `<span class="text-success fs-4"> ${listItem.innerHTML} 👾 </span>`
+    }
+
+}
+
+changeElementsByName();
+
+//----------------Uso del selector universal---------------------
+// querySelector
+
+const findElementByQuerySelector = () =>{
+    //const element = document.querySelector( "#title" ); // Seleccionar por ID
+    //const element = document.querySelector( ".text-warning" ); // Seleccionar por tag
+    //const element = document.querySelector( "ul" ); // Seleccionar por tag
+    const element = document.querySelector( "h1 span" ); // Seleccionar por tag
+    console.log( element ); 
+
+
+}
+
+findElementByQuerySelector();
+
+//-------------- Crear nuevo elemento HTML -----------------
+// appendChild
+
+const newElement = () => {
+    const newElement = document.createElement("div"); // <p></p>
+
+    newElement.innerHTML = `La CH30 le gusta: 
+    <ul>
+        <li>El helado</li>
+        <li>El chisme</li>
+        <li>Los corridos tumbados</li>
+        <li>Escuchar a Mau</li>
+        <li>Ser mejores amigos de Anneth</li>
+    </ul>
+    `;
+
+    const descriptionCh30 = document.getElementById("#descriptionCh30");
+    descriptionCh30.appendChild( newElement );
+}
+
+newElement();
