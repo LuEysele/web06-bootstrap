fc61e22934e54d3971adafa5761318125c0134e0
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-16 10:29:42 -0600

    ci: programación de secuencias

---
+++ assets/sesion-js/js07-asincronia.js
@@ -0,0 +1,16 @@
+console.log("Sesión JS07 - Asincronía");
+//----------------- Programación sincrónica (secuencial)-------------------
+
+const primerPaso = () => {
+    console.log("01 - Inicio de mi programa");
+}
+const segundoPaso = () => {
+    console.log("02 - Desarrollo de mi programa");
+}
+const tercerPaso = () => {
+    console.log("03 - Fin de mi programa");
+}
+
+primerPaso();
+segundoPaso();
+tercerPaso();
