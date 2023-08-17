fce0b154f86610943e0902b9556bbc016986b4a9
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-17 12:44:53 -0600

    ci: corrigiendo nombre de archivo de sesión

--- assets/sesion-js/js08.js
+++ assets/sesion-js/js08.js
@@ -1,1 +1,25 @@
 console.log("Sesión JS08 - POO");
+
+const registerForm = document.forms["registerForm"];
+
+registerForm.addEventListener( "submit", ( event )=>{
+    event.preventDefault(); 
+
+    const user = {
+        fullname: registerForm.elements["fullname"].value, 
+        email : registerForm.elements["email"].value, 
+        password : registerForm.elements["password"].value,        
+    }
+    console.table(user);
+    postUser(user);
+    // convirtiendo el objeto a formato JSON
+    localStorage.setItem("user", JSON.stringify(user));
+}  );
+
+const postUser = async ( userData) =>{
+    const url = "https://regres.in/api/register";
+
+    const response = await fetch( url, {} )
+    method: "POST", // POST, PUT, DELETE, GET
+    body: JSON.stringify(userData), //datos del usuario
+}
